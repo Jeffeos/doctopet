03e829649670ba06aa9c04eb8d9f98b4f367aa3a
Jeffeos <geoffrey.delenne@gmail.com>
2020-06-24 19:38:04 +0200

    project initialization

---
+++ src/Controller/AppController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
